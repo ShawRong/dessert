2fc549fda18b77dfdb38fa7e1dede0d8f0b6e32f
ShawRong <905869875@qq.com>
2020-12-10 17:35:11 +0800

    second commit

--- main.go
+++ main.go
@@ -2,21 +2,47 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
 
 	"github.com/kataras/iris/v12"
 )
 
+type Response struct {
+	Status bool        `json:"status"`
+	Data   interface{} `json:"data"`
+}
+
 func main() {
-	app := iris.New() // 实例一个iris对象
-	app.RegisterView(iris.HTML("./view", ".html"))
-	app.HandleDir("/static", iris.Dir("./view/static"))
+	app := iris.New()                                   // 实例一个iris对象
+	app.RegisterView(iris.HTML("./view", ".html"))      //资源库
+	app.HandleDir("/static", iris.Dir("./view/static")) //静态文件
 
 	//配置路由
 	app.Get("/", func(ctx iris.Context) {
 		ctx.View("index.html")
 	})
 
-	app.Post("/temp", UserLogin)
+	//用户登录
+	app.Post("/login", UserLogin)
+
+	//用户登录成功请求下一个界面
+	// /user/{name}/home
+	userRouter := app.Party("/user")
+	userRouter.Get("/{name:string}/home", func(ctx iris.Context) {
+		name := ctx.Params().Get("name")
+		ctx.Writef("%s", name)
+		ctx.View("user.html")
+	})
+
+	// /user/{name}/getname
+	userRouter.Get("/{name:string}/getname", func(ctx iris.Context) {
+		name := ctx.Params().Get("name")
+		_, _ = ctx.JSON(Response{Status: true, Data: name})
+	})
+
+	// /user/{name}/getquestion
+	userRouter.Get("/{name:string}/getquestion", RandGenerate)
 
 	// 路由分组
 	party := app.Party("/hello")
@@ -30,9 +56,10 @@ func main() {
 	// 监听地址:本服务器上任意id端口8085,设置字符集utf8
 }
 
+//login
 type User struct {
-	Username string `json:"username"`
-	Pwd      string `son:"pwd"`
+	Username string `json:"usrname"`
+	Pwd      string `json:"pwd"`
 }
 
 func UserLogin(ctx iris.Context) {
@@ -50,4 +77,27 @@ func UserLogin(ctx iris.Context) {
 		ctx.WriteString("Bad")
 	}
 	return
+}
+
+type Question struct {
+	X   string `json:"x"`
+	Y   string `json:"y"`
+	Sig string `json:"sig"`
+	Res string `json:"res"`
+}
+
+func RandGenerate(ctx iris.Context) {
+	x := rand.Intn(100)
+	y := rand.Intn(100)
+	sig := rand.Intn(2) //0 for + 1 for -
+	res := rand.Intn(2)
+	var mark string
+	if sig == 0 {
+		res = x + y
+		mark = "+"
+	} else {
+		res = x - y
+		mark = "-"
+	}
+	_, _ = ctx.JSON(Question{X: strconv.Itoa(x), Y: strconv.Itoa(y), Sig: mark, Res: strconv.Itoa(res)})
 }
